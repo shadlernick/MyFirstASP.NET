898917340bcf343bee4ad4d4d73dce2709633099
shadlernick <shadlernick99@gmail.com>
2020-01-19 01:11:27 +0200

    5th lesson ASP.NET - bootstrap + css/js/img

--- MyFirstASP.NET/Data/mocks/MockCars.cs
+++ MyFirstASP.NET/Data/mocks/MockCars.cs
@@ -12,9 +12,11 @@ namespace MyFirstASP.NET.Data.mocks
         private readonly ICarsCategory _categoryCars = new MockCategory();
         public IEnumerable<Car> Cars 
         { 
-            get => new List<Car>
+            get => 
+                new List<Car>
             {
-                new Car{Name="Tesla", ShortDesc="", LongDesc="", Available=true, Img="", Price=45000, IsFavourite=true, Category = _categoryCars.AllCategories.First()}
+                new Car{Name="Tesla", ShortDesc="Топ тачка", LongDesc="", Available=true, Img="/img/tesla_picture.jpg", Price=45000, IsFavourite=true, Category = _categoryCars.AllCategories.First()},
+                new Car{Name="Ford", ShortDesc="Тоже топ тачка", LongDesc="", Available=true, Img="/img/ford_picture.jpeg", Price=45000, IsFavourite=true, Category = _categoryCars.AllCategories.First()}
             };  
         }
         public IEnumerable<Car> FavCars { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
