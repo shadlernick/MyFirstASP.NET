acc9556446db2b5bdcb3352f2c7ea94f0ab77135
shadlernick <shadlernick99@gmail.com>
2020-01-22 07:21:48 +0200

    8th lesson, main page added

---
+++ MyFirstASP.NET/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstASP.NET.Data.Interfaces;
+using MyFirstASP.NET.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASP.NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IAllCars _carRep;
+
+        public HomeController(IAllCars carRep)
+        {
+            _carRep = carRep;
+        }
+
+        public ViewResult Index()
+        {
+            var homeCars = new HomeViewModel
+            {
+                FavCars = _carRep.FavCars
+            };
+            return View(homeCars);
+        }
+    }
+}
